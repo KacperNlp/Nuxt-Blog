8d2a2d5c305a09f4468eb0e5c4f4a72ef13fface
KacperNlp <kacper.nlp@wp.pl>
2025-05-24 20:45:22 +0200

    feat: remove nestjs for project and use building server in nuxt - #1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE
+    }
+  },
   css: ['~/assets/css/main.css'],
 
   modules: [
